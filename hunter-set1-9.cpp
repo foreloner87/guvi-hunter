3346758da0c115315e218c7730200a72baf9fa32
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-01-29 18:16:37 +0530

    Update hunter-set1-9.cpp

--- hunter-set1-9.cpp
+++ hunter-set1-9.cpp
@@ -12,8 +12,18 @@ int main()
 	for(int i=0;i<n;i++)
 	cin>>a[i];
 	for(int i=0;i<n;i++)
-	for(int j=i+1;j<n;j++)
-	if(a[i]+a[j]==0)
-	cout<<a[i]<<' '<<a[j];
-	return 0;
+	 for(int j=i+1;j<n;j++)
+	  if(a[i]+a[j]==0)
+	   {
+		cout<<a[i]<<' '<<a[j]<<endl;
+		return 0;
+	   }
+   for(int k=1;k<100;k++)
+	for(int i=0;i<n;i++)
+	 for(int j=i+1;j<n;j++)
+	    if((a[i]+a[j]==k)||(a[i]+a[j]==-k))
+	    {
+		 cout<<a[i]<<' '<<a[j]<<endl;
+		 return 0;
+	    }
 }
