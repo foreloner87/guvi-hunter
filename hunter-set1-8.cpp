88af745e8b1cfcfaa4ed2e0e70ba14e8b9502728
foreloner87 <34986630+foreloner87@users.noreply.github.com>
2019-01-29 17:59:26 +0530

    Update hunter-set1-8.cpp

--- hunter-set1-8.cpp
+++ hunter-set1-8.cpp
@@ -8,17 +8,13 @@ int main()
 	cout<<"Invalid input";
 	return 0;
 	}
-	int a[n],i,j,k,cj=0;
+	int a[n],i,j,k;
 	for(i=0;i<n;i++)
 	cin>>a[i];
 	for(i=0;i<n;i++)
 	 for(j=i+1;j<n;j++)
 	  for(k=j+1;k<n;k++)
-	   if((a[i]+a[j]==a[k])&&(cj!=j))
-	    {	
-	     cout<<a[i]<<' '<<a[j]<<' '<<a[k]<<endl;
-	     cj=j;
-	    }
+	   if(a[i]+a[j]==a[k])
+	     cout<<a[i]<<' '<<a[j++]<<' '<<a[k]<<endl;
 	return 0;
 }
-
